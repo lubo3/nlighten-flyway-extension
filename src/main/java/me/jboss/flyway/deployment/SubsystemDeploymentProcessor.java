372fe4a79cf41f601c0d01273e2db16011aae5b1
lubo3 <korenko.lubo@gmail.com>
2016-12-01 23:31:52 +0100

    added datasource and flyway implementation

--- src/main/java/me/jboss/flyway/deployment/SubsystemDeploymentProcessor.java
+++ src/main/java/me/jboss/flyway/deployment/SubsystemDeploymentProcessor.java
@@ -1,7 +1,10 @@
 package me.jboss.flyway.deployment;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
@@ -21,31 +24,73 @@ import org.jboss.vfs.VirtualFile;
  * <li>Replacing a deployment descriptor with a transformed version of that descriptor</li>
  * <li>Removing a deployment descriptor to prevent it from being processed</li>
  * </ol>
- * 
+ * .
+ *
  * @author lubo
  */
 public class SubsystemDeploymentProcessor implements DeploymentUnitProcessor {
+
+  /** The migration folder path. */
+  private static String MIGRATION_FOLDER_PATH = "WEB-INF/classes/db/migration";
+
+  /** The persistence xml path. */
+  private static String PERSISTENCE_XML_PATH = "src/main/resources/persistence.xml";
+
+  /** The datasource element. */
+  private static String DATASOURCE_ELEMENT = "jta-data-source";
+
+  /** The datasource regex. */
+  private static String DATASOURCE_REGEX = "</?jta-data-source>";
 
   /** {@inheritDoc} */
   @Override
   public void deploy(DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
     ResourceRoot root = phaseContext.getDeploymentUnit().getAttachment(Attachments.DEPLOYMENT_ROOT);
-    try {
-      InitialContext initialContext = new InitialContext();
-      DataSource dataSource =
-          (DataSource) initialContext.lookup("java:jboss/datasources/ExampleDS");
-      VirtualFile migrationFolder = root.getRoot().getChild("WEB-INF/classes/db/migration");
-      if (migrationFolder.exists()) {
+    VirtualFile migrationFolder = root.getRoot().getChild(MIGRATION_FOLDER_PATH);
+    if (migrationFolder.exists()) {
+      DataSource dataSource = getDatasource(root);
+      if (dataSource != null) {
         Flyway flyway = new Flyway();
         flyway.setDataSource(dataSource);
         flyway.migrate();
       }
-    } catch (NamingException e) {
-      e.printStackTrace();
     }
   }
 
   /** {@inheritDoc} */
   @Override
   public void undeploy(DeploymentUnit context) {}
+
+  /**
+   * Gets the datasource.
+   *
+   * @param root the root
+   * @return the datasource
+   */
+  private DataSource getDatasource(ResourceRoot root) {
+    VirtualFile persistenceXml = root.getRoot().getChild(PERSISTENCE_XML_PATH);
+    DataSource dataSource = null;
+    try {
+      InitialContext initialContext = new InitialContext();
+      if (persistenceXml.exists()) {
+        String line;
+        InputStream is = persistenceXml.openStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String datasourceName = null;
+        while ((line = br.readLine()) != null) {
+          if (line.contains(DATASOURCE_ELEMENT)) {
+            datasourceName = line.replaceAll(DATASOURCE_REGEX, "").trim();
+            break;
+          }
+        }
+        if (datasourceName != null && !datasourceName.isEmpty()) {
+          dataSource = (DataSource) initialContext.lookup(datasourceName);
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return dataSource;
+  }
+
 }
