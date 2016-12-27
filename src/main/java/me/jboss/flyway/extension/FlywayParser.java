f76e71511c409d65b7b36b8711dee1d59fe3d9a2
lubo3 <korenko.lubo@gmail.com>
2016-12-27 10:39:11 +0100

    Add datasource flyway db (#3)
    * added datasource and flyway implementation
    
    * added datasource and flyway implementation

--- src/main/java/me/jboss/flyway/extension/FlywayParser.java
+++ src/main/java/me/jboss/flyway/extension/FlywayParser.java
@@ -2,11 +2,8 @@ package me.jboss.flyway.extension;
 
 import static org.jboss.as.controller.PersistentResourceXMLDescription.builder;
 
-import org.jboss.as.controller.AttributeDefinition;
 import org.jboss.as.controller.PersistentResourceXMLDescription;
 import org.jboss.as.controller.PersistentResourceXMLParser;
-import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
-import org.jboss.dmr.ModelType;
 
 /**
  * The subsystem parser, which uses stax to read and write to and from xml.
@@ -15,14 +12,9 @@ import org.jboss.dmr.ModelType;
  */
 class FlywayParser extends PersistentResourceXMLParser {
 
-  /** The Constant PATH. */
-  protected static final AttributeDefinition PATH =
-      new SimpleAttributeDefinitionBuilder(FlywayExtension.PATH, ModelType.STRING)
-          .setAllowExpression(true).build();
-
   /** The Constant xmlDescription. */
   private static final PersistentResourceXMLDescription xmlDescription =
-      builder(FlywayRootDefinition.INSTANCE, FlywayExtension.NAMESPACE).addAttributes(PATH).build();
+      builder(FlywayRootDefinition.INSTANCE, FlywayExtension.NAMESPACE).build();
 
   /** {@inheritDoc} */
   @Override
